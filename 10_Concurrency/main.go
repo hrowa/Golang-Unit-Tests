7d4ecd068f44a4b2fa0500b024742c8bbba917cf
hrowa <adjidomo@gmail.com>
2022-09-14 16:09:11 +0700

    second commit

---
+++ 10_Concurrency/main.go
@@ -0,0 +1,36 @@
+package concurrency
+
+import "net/http"
+
+func CheckWebsite(url string) bool {
+	response, err := http.Head(url)
+	if err != nil {
+		return false
+	}
+
+	return response.StatusCode == http.StatusOK
+}
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
